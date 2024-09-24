40588330f1961cbb707b9090ec1b46df22ca2156
Milan Sobat <milansobat03@gmail.com>
2024-09-24 19:41:52 -0400

    Fixing intersection tests

--- linked-list.test.js
+++ linked-list.test.js
@@ -672,30 +672,22 @@ describe('Linked List Tests', () => {
     });
 
     it('should determine if lists intersect and return the intersecting node', () => {
-        const nodeToGet = new ListNode(7);
-        nodeToGet.next = new ListNode(1);
-        nodeToGet.next.next = new ListNode(2);
-        nodeToGet.next.next.next = new ListNode(3);
+        const sharedNode = new ListNode(7);
+        sharedNode.next = new ListNode(1);
+        sharedNode.next.next = new ListNode(2);
+        sharedNode.next.next.next = new ListNode(3);
         
         linkedList.addToEnd(6);
         linkedList.addToEnd(1);
         const l1EndNode = linkedList.getNodeAtIdx(1);
-        l1EndNode.next = nodeToGet;
-        // linkedList.addToEnd(7);  
-        // linkedList.addToEnd(1);  
-        // linkedList.addToEnd(2);  
-        // linkedList.addToEnd(3);  
+        l1EndNode.next = sharedNode;
 
         let secondList = new LinkedList();
         secondList.addToEnd(6);
         secondList.addToEnd(9);
-        const l2EndNode = linkedList.getNodeAtIdx(1);
-        l2EndNode.next = nodeToGet;
-        // secondList.addToEnd(7);
-        // secondList.addToEnd(1);
-        // secondList.addToEnd(2);
-        // secondList.addToEnd(3);
-        
+        const l2EndNode = secondList.getNodeAtIdx(1);
+        l2EndNode.next = sharedNode;
+
         const nodeToTest = linkedList.getNodeAtIdx(2);
 
         let intersectingNode = linkedList.intersection(secondList);
@@ -731,28 +723,18 @@ describe('Linked List Tests', () => {
     });
 
     it('should determine if lists intersect at head and return the intersecting node', () => {
-        linkedList.addToEnd(6);
-        linkedList.addToEnd(1);
-        linkedList.addToEnd(7);  
-        linkedList.addToEnd(1);  
-        linkedList.addToEnd(2);  
-        linkedList.addToEnd(3);  
+        const sharedNode = new ListNode(7);
+        sharedNode.next = new ListNode(1);
+        sharedNode.next.next = new ListNode(2);
+        sharedNode.next.next.next = new ListNode(3);
+        
+        linkedList.head = sharedNode;
 
         let secondList = new LinkedList();
-        secondList.addToEnd(6);
-        secondList.addToEnd(1);
-        secondList.addToEnd(7);
-        secondList.addToEnd(1);
-        secondList.addToEnd(2);
-        secondList.addToEnd(3);
-
-        const nodeToGet = new ListNode();
-        nodeToGet.data = 6;
-        nodeToGet.next = new ListNode(1);
-        const nodeToTest = linkedList.getNode(nodeToGet);
+        secondList.head = sharedNode;
 
         let intersectingNode = linkedList.intersection(secondList);
-        expect(intersectingNode).toEqual(nodeToTest);
+        expect(intersectingNode).toEqual(linkedList.head);
     });
 
     it('should determine if lists intersect and return the intersecting node, if source list is shorter', () => {
