4f71a2b54d21e3ed7c2ac8b3ecd1571ba2281dfb
Milan Sobat <milansobat03@gmail.com>
2024-09-27 11:05:00 -0400

    Added stack implementation and tests

--- stack.js
+++ stack.js
@@ -1,5 +1,33 @@
 class Stack {
+    constructor() {
+        this._items = new Array();
+    }
+    
+    get items() {
+        return this._items;
+    }
 
+    pop() {
+        if (this.isEmpty()) {
+            throw new Error("Stack is empty.");
+        }
+        this._items.pop();
+    }
+
+    push(item) {
+        this._items.push(item);
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw new Error("Stack is empty.");
+        }
+        return this._items[this._items.length - 1];
+    }
+
+    isEmpty() {
+        return this._items.length === 0
+    }
 }
 
 module.exports = { Stack };
--- stack.test.js
+++ stack.test.js
@@ -1,5 +1,43 @@
-const Stack = require('./stack');
+const { Stack } = require('./stack');
 
 describe('Stack Tests', () => {
+    it('should push an item onto the stack', () => {
+        const stack = new Stack();
+        stack.push(3);
 
+        expect(stack.items).toBeDefined();
+        expect(stack.items).toHaveLength(1);
+    });
+    
+    it('should allow to peek item at top of stack', () => {
+        const stack = new Stack();
+        expect(() => { stack.peek() }).toThrow('Stack is empty.');
+
+        stack.push(1);
+        stack.push(2);
+
+        expect(stack.peek()).toEqual(2);
+    });
+    
+    it('should pop an item from the top of the stack', () => {
+        const stack = new Stack();
+        expect(() => { stack.pop() }).toThrow('Stack is empty.');
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+
+        expect(stack.items).toHaveLength(2);
+        expect(stack.peek()).toEqual(2);
+    });
+
+    it('should return true if stack is empty and false if not', () => {
+        const stack = new Stack();
+        expect(stack.isEmpty()).toEqual(true);
+
+        stack.push(1);
+
+        expect(stack.isEmpty()).toEqual(false);
+    });
 });
