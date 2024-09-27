c978f765ca82d1c68d27fe2d04045a227d3d4eac
Milan Sobat <milansobat03@gmail.com>
2024-09-27 17:30:32 -0400

    Prelim min functionality

--- stack.js
+++ stack.js
@@ -1,20 +1,42 @@
 class Stack {
     constructor() {
         this._items = new Array();
+        this._minStack = new Array();
+        this._min = null;
     }
     
     get items() {
         return this._items;
     }
 
+    get min() {
+        return this._min;
+    }
+
     pop() {
         if (this.isEmpty()) {
             throw new Error("Stack is empty.");
+        }
+        if (this._min && this._min === this._items[this._items.length - 1] && 
+            this._minStack.length > 0) {
+            this._minStack.pop();
+            this._min = this._minStack[this._minStack - 1];
         }
         return this._items.pop();
     }
 
     push(item) {
+        if (this.isEmpty()) {
+            if (typeof item === 'number' && !isNaN(item)) {
+                this._minStack.push(item);
+                this._min = item;
+            }
+        } else {
+            if (typeof item === 'number' && !isNaN(item) && item < this._min) {
+                this._minStack.push(item);
+                this._min = item;
+            }
+        }
         this._items.push(item);
     }
 
@@ -26,7 +48,7 @@ class Stack {
     }
 
     isEmpty() {
-        return this._items.length === 0
+        return this._items.length === 0;
     }
 }
 
