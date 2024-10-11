ae7b9fe7a06b7f6e81d9cd862d2e7db427c72ea9
Milan Sobat <milansobat03@gmail.com>
2024-10-11 11:16:11 -0400

    Work on problem

--- evaluate-mathematical-expression.js
+++ evaluate-mathematical-expression.js
@@ -1,5 +1,43 @@
 const calc = function(expression) {
+    expression = expression.replace(/\s/g, '');
+    let operatorStack = [];
+    let operandStack = [];
+    let constructedNum = '';
+    
+    let inBracketSeq = false;
+    for (let i = 0; i < expression.length; ++i) {
+        let char = expression[i];
+        if (inBracketSeq || char === '(') {
+            inBracketSeq = true;
+            if (isNumberOrDecimal(char)) {
+                constructedNum += char;
+                let next = expression[i + 1];
+                if (!isNumberOrDecimal(next)) {
+                    operandStack.push(+constructedNum);
+                    constructedNum = '';
+                }
+            } // TODO: continue if operator etc.
+        }
 
+        if (isNumberOrDecimal(char)) {
+            constructedNum += char;
+            let next = expression[i + 1];
+            if (!isNumberOrDecimal(next)) {
+                operandStack.push(+constructedNum);
+                constructedNum = '';
+            }
+        } else if (isOperator(char)) {
+            operatorStack.push(char);
+        }
+    }
 }
 
-module.exports = { calc };
+const isNumberOrDecimal = function(char) {
+    return /^[0-9.]$/.test(char);
+}
+
+const isOperator = function(char) {
+    return /^[+\-*/]$/.test(char);
+}
+
+module.exports = { calc, isNumberOrDecimal, isOperator };
--- evaluate-mathematical-expression.test.js
+++ evaluate-mathematical-expression.test.js
@@ -1,5 +1,44 @@
-const { calc } = require('./evaluate-mathematical-expression');
+const { calc, isNumberOrDecimal, isOperator } = require('./evaluate-mathematical-expression');
 
 describe('Evaluate Mathematical Expression Tests', () => {
 
+});
+
+describe('Is Number or Decimal Tests', () => {
+    it('should identify a number', () => {
+        expect(isNumberOrDecimal('1')).toBe(true);
+    });
+    
+    it('should identify a decimal', () => {
+        expect(isNumberOrDecimal('.')).toBe(true);
+    });
+
+    it('should identify when it is not a number or decimal', () => {
+        expect(isNumberOrDecimal('a')).toBe(false);
+        expect(isNumberOrDecimal('*')).toBe(false);
+    });
+});
+
+describe('Is Operator Tests', () => {
+    it('should identify when it is addition', () => {
+        expect(isOperator('+')).toBe(true);
+    });
+    
+    it('should identify when it is subtraction', () => {
+        expect(isOperator('-')).toBe(true);
+    });
+
+    it('should idenitfy when it is multiplication', () => {
+        expect(isOperator('*')).toBe(true);
+    });
+
+    it('should identify when it is division', () => {
+        expect(isOperator('/')).toBe(true);
+    });
+
+    it('should idenitfy when it is not one of the acceptable operators', () => {
+        expect(isOperator('a')).toBe(false);
+        expect(isOperator('$')).toBe(false);
+        expect(isOperator('4')).toBe(false);
+    });
 });
